166bf2be60b750d9edb6a2c8386dd13e29202139
Yuusuke Yoda <ubuntu@ip-172-31-24-201.ap-northeast-1.compute.internal>
2020-08-27 00:51:08 +0000

    rails g controller StaticPages home help

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,9 @@
+class StaticPagesController < ApplicationController
+  def home
+    #=> "app/views/リソース名/アクション名.html.erb"
+    #=> "qpp/views/static_pages/home.html.erb"
+  end
+
+  def help
+  end
+end
