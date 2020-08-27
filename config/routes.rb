482ed873556007b499bb68366575f51ed2c35bd1
Yuusuke Yoda <ubuntu@ip-172-31-24-201.ap-northeast-1.compute.internal>
2020-08-27 04:25:26 +0000

    Finish ch.03

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  #  root 'application#hello'
+  
   get 'static_pages/home'
+  # => staticpages#home
+  
   get 'static_pages/help'
+  # => staticpages#help
+  
+  get 'static_pages/about'
+  
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+ 
 end
