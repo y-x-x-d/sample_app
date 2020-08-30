61448183c58ec07e2b59e6bfe3f97e23d342cf66
Yuusuke Yoda <ubuntu@ip-172-31-24-201.ap-northeast-1.compute.internal>
2020-08-30 07:11:27 +0000

    Finish ch.05

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,4 +10,7 @@ class StaticPagesController < ApplicationController
   def about
      #=> "qpp/views/static_pages/about.html.erb"
   end
+  
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,17 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   #  root 'application#hello'
   
-  get 'static_pages/home'
-  # => staticpages#home
   
-  get 'static_pages/help'
+  get '/help',  to:'static_pages#help'
   # => staticpages#help
   
-  get 'static_pages/about'
+  get '/about',  to:'static_pages#about'
   #ルーティングにともなったURLが使えるようになる
+  
+  get '/contact',  to:'static_pages#contact'
+  
+  get '/signup', to:'users#new'
+  # get 'users/new'
   
   
   
